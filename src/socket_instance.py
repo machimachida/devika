16e905886d83a2d16b4b16cc3989caaa951ee0de
Ryo Machida <ryo.machida@biprogy.com>
2024-05-08 12:05:32 +0900

    WIP: socket_instanceのグローバル変数loggerの変更作業中

--- src/socket_instance.py
+++ src/socket_instance.py
@@ -1,17 +1,19 @@
 # socketio_instance.py
 from flask_socketio import SocketIO
 from src.logger import Logger
-socketio = SocketIO(cors_allowed_origins="*", async_mode="gevent")
-
-logger = Logger()
 
 
-def emit_agent(channel, content, log=True):
-    try:
-        socketio.emit(channel, content)
-        if log:
-            logger.info(f"SOCKET {channel} MESSAGE: {content}")
-        return True
-    except Exception as e:
-        logger.error(f"SOCKET {channel} ERROR: {str(e)}")
-        return False
+class EmitAgent:
+    def __init__(self):
+        self.socketio = SocketIO(cors_allowed_origins="*", async_mode="gevent")
+        self.logger = Logger()
+
+    def emit_content(self, channel, content, log=True):
+        try:
+            self.socketio.emit(channel, content)
+            if log:
+                self.logger.info(f"SOCKET {channel} MESSAGE: {content}")
+            return True
+        except Exception as e:
+            self.logger.error(f"SOCKET {channel} ERROR: {str(e)}")
+            return False
