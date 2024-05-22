91a4999190e344be3031a7e291f13b1d07b2db3a
Ryo Machida <ryo.machida@biprogy.com>
2024-05-22 17:22:11 +0900

    Add similarity finder

--- src/agents/coder/similarity_finder.py
+++ src/agents/coder/similarity_finder.py
@@ -1,4 +1,5 @@
 from jinja2 import Environment, BaseLoader
+import json
 from pathlib import Path
 
 from src.config import Config
@@ -18,28 +19,43 @@ class SimilarityFinder:
         with open(parent.joinpath("similarity.jinja2"), 'r') as file:
             self.prompt_template = file.read().strip()
 
-    def render(self, step_by_step_plan: str, user_context: str, methods: list[str]) -> str:
+    def render(self, step_by_step_plan: str, methods: list[str]) -> str:
         env = Environment(loader=BaseLoader())
         template = env.from_string(self.prompt_template)
         return template.render(
             step_by_step_plan=step_by_step_plan,
-            user_context=user_context,
             methods=methods,
         )
 
-    def validate_response(self, response: str):
-        return response
+    def validate_response(self, response: str) -> list[str]:
+        """
+        以下の形式でレスポンスが返却されていることを確認し、methodsのもつlistを返却する
+
+        ```
+        {
+          "methods": [
+            "com.example.task.TaskService#createTask",
+            "com.example.user.UserService#createUser"
+          ]
+        }
+        }
+        ```
+        """
+
+        try:
+            # \nやスペースの他に、```がJSONの前後にある場合があるので、削除する
+            stripped = response.strip().replace('```', '')
+            response_dict = json.loads(stripped)
+            return response_dict.get('methods', [])
+        except json.JSONDecodeError:
+            self.logger.error(f'Failed to decode response as JSON: {response}')
+            return []
 
     @retry_wrapper
-    def execute(self, step_by_step_plan: str, user_context: str, project_name: str) -> str:
+    def execute(self, step_by_step_plan: str, project_name: str) -> list[str]:
         methods_dict: dict[str, str] = ReadCode(project_name).get_methods_names()
         methods: list[str] = list(methods_dict.keys())
-        prompt = self.render(step_by_step_plan=step_by_step_plan, user_context=user_context, methods=methods)
+        prompt = self.render(step_by_step_plan=step_by_step_plan, methods=methods)
         response = self.llm.inference(prompt, project_name)
 
-        valid_response = self.validate_response(response)
-
-        if not valid_response:
-            return ''
-
-        return valid_response
+        return self.validate_response(response)
